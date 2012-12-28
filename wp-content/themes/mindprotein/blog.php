bdfdcad58460e26e73dd33b4fb51e50e5dfcbbb9
bjaalberts <bjaalberts@gmail.com>
2012-12-28 09:28:27 -0800

    Friday morning commit for Jesse to add functionality

--- wp-content/themes/mindprotein/blog.php
+++ wp-content/themes/mindprotein/blog.php
@@ -55,7 +55,7 @@ get_header(); ?>
 			</div>
 			<div class="row">
 				<div class="span3">
-					<img src="<?php bloginfo('template_directory'); ?>/img/blogPicture.png">
+					<img src="<?php bloginfo('template_directory');?>/img/blogPicture.png">
 				</div>
 				<div class="span4 blog-content">
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
@@ -63,6 +63,11 @@ get_header(); ?>
 				</div>
 				<div class="span2">
 					Social
+				</div>
+			</div>
+			<div class="row">
+				<div class="span12">
+					<img src="<?php bloginfo('template_directory');?>/img/blogShadow.png" />
 				</div>
 			</div>
 	</div>
