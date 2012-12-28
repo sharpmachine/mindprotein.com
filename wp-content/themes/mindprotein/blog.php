9776fc47edaf897ae2e529614291b0bf12d7eea7
bjaalberts <bjaalberts@gmail.com>
2012-12-28 09:34:16 -0800

    test

--- wp-content/themes/mindprotein/blog.php
+++ wp-content/themes/mindprotein/blog.php
@@ -5,44 +5,46 @@
 get_header(); ?>
 
 <article>
-	
-<div class="blog-one">
-	<div class="container">
-		<div class="row">
-			<div class="span6">
-				<img src="<?php bloginfo('template_directory');?>/img/trendingRightNow.png" />
-				<h2>Business Development 101: Hiring Development</h2>
-				<div class="author-date">
-					By <span>Ralph Plumb</span>, October 1, 2012
+<div class="container">
+	<div class="row">
+		<div class="span6">
+
+			<div class="blog-one">
+				<div class="container">
+					<div class="row">
+						<div class="span6">
+							<img src="<?php bloginfo('template_directory');?>/img/trendingRightNow.png" />
+							<h2>Business Development 101: Hiring Development</h2>
+							<div class="author-date">
+								By <span>Ralph Plumb</span>, October 1, 2012
+							</div>
+							<div class="social-media">
+								
+							</div>
+							<div class="blog-content">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud. 
+								<span>Read More >></span>
+							</div>
+						</div>
+						<div class="span3 discussions">
+							<h3>Populare Community Discussions</h3>
+							<div class="discuss-list">
+								<ol>
+									<li>Development 101: Hiring a Designer</li>
+									<li>Development 101: Hiring a Designer</li>
+									<li>Development 101: Hiring a Designer</li>
+									<li>Development 101: Hiring a Designer</li>
+									<li>Development 101: Hiring a Designer</li>
+									<li>Development 101: Hiring a Designer</li>
+								</ol>
+								<a class="btn btn-primary btn-medium">Join the Discussion |></a>
+							</div>
+						</div>
+					</div>
 				</div>
-				<div class="social-media">
+			</div>
+
 					
-				</div>
-				<div class="blog-content">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud. 
-					<span>Read More >></span>
-				</div>
-			</div>
-			<div class="span3 discussions">
-				<h3>Populare Community Discussions</h3>
-				<div class="discuss-list">
-					<ol>
-						<li>Development 101: Hiring a Designer</li>
-						<li>Development 101: Hiring a Designer</li>
-						<li>Development 101: Hiring a Designer</li>
-						<li>Development 101: Hiring a Designer</li>
-						<li>Development 101: Hiring a Designer</li>
-						<li>Development 101: Hiring a Designer</li>
-					</ol>
-					<a class="btn btn-primary btn-medium">Join the Discussion |></a>
-				</div>
-			</div>
-			<div class="span3">
-				<?php get_sidebar(); ?>
-			</div>
-		</div>
-	</div>
-</div>
 
 <div class="blog-two">
 	<div class="container">
@@ -75,7 +77,7 @@ get_header(); ?>
 
 </article>
 
-
+<?php get_sidebar(); ?>
 
 <section id="page" class="span8">
 
