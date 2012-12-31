08e7dd435fbb410ccb91b261129a8f35d952ce20
Jesse Kade <jessekade@Sharp-Machine.local>
2012-12-31 01:05:50 -0800

    working on client testimonies

--- wp-content/themes/mindprotein/case-studies.php
+++ wp-content/themes/mindprotein/case-studies.php
@@ -117,24 +117,17 @@ Template Name: Case Studies
 			</div>
 		</div>
 		<div class="row">
-			<div class="span4 rightSpace">
-				<img src="<?php bloginfo('template_directory')?>/img/clientTestimonials.png" alt="">
-				<h3>Bob McIntosh</h3>
-				<div class="subTitle">Redhead Rehabs</div>
-				<blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</blockquote>
-			</div>
-			<div class="span4 rightSpace">
-				<img src="<?php bloginfo('template_directory')?>/img/clientTestimonials.png" alt="">
-				<h3>Bob McIntosh</h3>
-				<div class="subTitle">Redhead Rehabs</div>
-				<blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</blockquote>
-			</div>
-			<div class="span4">
-				<img src="<?php bloginfo('template_directory')?>/img/clientTestimonials.png" alt="">
-				<h3>Bob McIntosh</h3>
-				<div class="subTitle">Redhead Rehabs</div>
-				<blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</blockquote>
-			</div>
+			<?php query_posts('post_type=client_testimonies&showposts=3'); ?>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<div class="span4">
+					<img src="<?php the_field('client_headshot') ?>" alt="">
+					<h3><?php the_title(); ?></h3>
+					<div class="subTitle"><?php the_field('company'); ?></div>
+					<blockquote><?php the_field('testimony') ?></blockquote>
+				</div>
+			<?php endwhile; else: ?>
+			<p>No testimonies at this time.</p>
+			<?php endif; ?>
 		</div>
 	</div>
 </div>
